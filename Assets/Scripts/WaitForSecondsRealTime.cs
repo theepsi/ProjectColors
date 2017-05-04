d183021bfb371ef316cc5a2ba25da2ddfb3f48de
Ennio Casas Puglielli <theepsi@gmail.com>
2017-05-04 15:08:51 +0200

    Add Custom YieldInstructrion

---
+++ Assets/Scripts/WaitForSecondsRealTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForSecondsRealTime : CustomYieldInstruction {
+
+    private float _waitTime;
+
+    public override bool keepWaiting
+    {
+        get { return Time.realtimeSinceStartup < _waitTime; }
+    }
+
+    public WaitForSecondsRealTime(float time)
+    {
+        _waitTime = Time.realtimeSinceStartup + time;
+    }
+}
